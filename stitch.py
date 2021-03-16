6d9e52000ce4ab135ce11359d549dc4fc5d3c4ce
kojiboji <kojiboji@gmail.com>
2021-03-15 23:07:06 -0700

    write to a video file "out.mavi" using VideoWriter

--- stitch.py
+++ stitch.py
@@ -5,14 +5,25 @@ import cv2
 import io
 import imutils
 
+
 def stitch(img_pair):
     stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
     ret = (status, stitched) = stitcher.stitch([img_pair[0], img_pair[1]])
     return ret
 
+
 def binary_to_cv(binaryfile):
     img = np.array(Image.open(io.BytesIO(binaryfile[1])).convert('RGB'))[:, :, ::-1].copy()
     return img
+
+
+# a must be smaller than the shape
+# write a into the 0,0,0 of a new shape
+# so that we can write frames of different sizes to the same video
+def zero_resize(a, shape):
+    ret = np.zeros(shape, dtype=a.dtype)
+    ret[0:a.shape[0], 0:a.shape[1], 0:a.shape[2]] = a
+    return ret
 
 
 if __name__ == '__main__':
@@ -27,9 +38,9 @@ if __name__ == '__main__':
     right_camera = cv2.VideoCapture('example_video/right/ny_right_short.mp4')
     left_feed = []
     right_feed = []
-    while(True):
+    while (True):
         ret, frame = left_camera.read()
-        if ret==True:
+        if ret == True:
             left_feed.append(frame)
         else:
             break
@@ -39,14 +50,23 @@ if __name__ == '__main__':
             right_feed.append(frame)
         else:
             break
+
     left_data = sc.parallelize(left_feed)
     right_data = sc.parallelize(right_feed)
     pairs = left_data.zip(right_data)
     processed = pairs.map(lambda p: stitch(p)).collect()
+
+    # height, width, depth
+    shape = (0, 0, 0)
     for (status, img) in processed:
-        if status==0:
-            cv2.imshow('image', img)
+        if status == 0:
+            shape = tuple([max(current, new) for current, new in zip(shape, img.shape)])
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    writer = cv2.VideoWriter("out.avi", fourcc, 24.0, (shape[1], shape[0]))
+    for (status, img) in processed:
+        if status == 0:
+            # cv2.imshow("image", zero_resize(img, shape))
+            writer.write(zero_resize(img, shape))
             cv2.waitKey(1)
-        else:
-            print("Error")
+    writer.release()
     exit(0)
